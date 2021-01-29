c8ae9a14c0d3aa895f68f98de571a42d10b27444
Sugandhraj Patel <psugandhraj@gmail.com>
2021-01-29 17:21:01 +0530

    Init commit

---
+++ src/emails/account.js
@@ -0,0 +1,22 @@
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+const sendWelcomeEmail =  (email, name) => {
+  sgMail.send({
+    to: email,
+    from: 'psugandhraj@gmail.com',
+    subject: 'Thanks for joining in!',
+    text: `Welcome to the app, ${name}. Let me know how you get along with the app.`   
+  })
+}
+const sendLeavingEmail = (email, name) => {
+  sgMail.send({
+    to: email,
+    from: 'psugandhraj@gmail.com',
+    subject: `Good bye ${name}`,
+    text: 'Is there anything we could have done to have kept you onboard.'
+  })
+}
+module.exports = {
+  sendWelcomeEmail,
+  sendLeavingEmail
+}  
---
+++ src/routers/task.js
@@ -0,0 +1,88 @@
+const express = require('express')
+const router = new express.Router()
+const auth = require('../middleware/auth')
+const Task = require('../models/task')
+const User = require('../models/user')
+router.post('/tasks', auth, async ( req, res ) => {
+    const task = new Task({ ...req.body, owner: req.user.id })
+    try{
+        await task.save()
+        res.status(201).send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+//GET /tasks?completed=true
+//GET /tasks?limit=10&skip=20
+//GET /tasks?sortBy=createdAt_asc
+router.get('/tasks', auth, async (req, res) => {
+    const match = {}
+    const sort = {}
+    if(req.query.completed){
+        match.completed = req.query.completed === 'true'
+    }
+    if(req.query.sortBy){
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+    }
+    try {
+        await req.user.populate({
+            path: 'tasks',
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip),
+                sort
+            }
+        }).execPopulate()
+        res.send(req.user.tasks) 
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+router.get('/tasks/:id', auth, async (req, res) => {
+    try {
+        const task = await Task.findOne({_id: req.params.id, owner: req.user._id})
+        if(!task){
+            return res.status(404).send()
+        }
+        res.send(task) 
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+router.patch('/tasks/:id', auth, async (req,res) =>{
+    const updates = Object.keys(req.body)
+    const isValidOperation = updates.every(update => ['description', 'completed'].includes(update))
+    if(!isValidOperation){
+        return res.status(400).send({error:"Invalid Updates"})
+    }
+    try{
+        const task = await Task.findOne({ _id: req.params.id, owner:req.user._id})
+        
+        if(!task){
+            return res.status(404).send()
+        }
+
+        updates.forEach(update=> task[update] = req.body[update])
+        await task.save()
+        
+        res.send(task)
+    } catch (e){
+        res.status(400).send(e)
+    }
+})
+router.delete('/tasks/:id', auth, async (req,res) => {
+    try {
+        const task = await Task.findOneAndDelete({ _id:req.params.id, owner:req.user.id})
+        if(!task){
+            return res.status(404).send()
+        } 
+        res.send(task);
+    } catch (error) {
+        res.status(500).send(error)
+    }
+    
+})
+
+module.exports = router
